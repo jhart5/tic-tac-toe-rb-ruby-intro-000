9e5d6afe64305b5115af371ff83865e669cc508c
josh hart <joshua.hart@protonmail.com>
2019-09-16 10:37:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+def input_to_index(input)
+  input.to_i - 1
+end
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
