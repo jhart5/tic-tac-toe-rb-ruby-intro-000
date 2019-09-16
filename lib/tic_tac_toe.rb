393e182ce28b80679b1eb068048a93c83b7feb8b
josh hart <joshua.hart@protonmail.com>
2019-09-16 11:50:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ end
 def turn_count(board)
   counter = 0
   board.all? do |position|
-    if position != " " && position != nil
+    if ((position == "X") || (position == "O"))
       counter += 1
     end
   end
