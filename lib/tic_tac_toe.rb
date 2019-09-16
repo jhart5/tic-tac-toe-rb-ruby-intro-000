63d33bea5ab787180f31f0ddabefdcc33146ada2
josh hart <joshua.hart@protonmail.com>
2019-09-16 11:07:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,14 @@ turn_count(board)
   counter = board.length
 end
 
+current_player(board)
+  if turn_count(board) % 2 = 0
+    "X"
+  else
+    "O"
+  end
+end
+
 
 WIN_COMBINATIONS = [
   [0,1,2],
